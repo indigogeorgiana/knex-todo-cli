e9e32e1e4ec52e414184253b03fb7c727d7209f8
Sungjun Park <sjpark731015@gmail.com>
2018-06-11 17:01:21 +1200

    All exercises done

--- seeds/test-tasks.js
+++ seeds/test-tasks.js
@@ -4,9 +4,10 @@ exports.seed = function (knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('todos').insert([
-        {id: 1, task: 'eat breakfast'},
-        {id: 2, task: 'brush teeth'},
-        {id: 3, task: 'have a shower'}
+        {id: 1, task: 'eat breakfast', completed: 'Null', priority: 1},
+        {id: 2, task: 'brush teeth', completed: 'Y', priority: 2},
+        {id: 3, task: 'have a shower', completed: 'Y', priority: 3},
+        {id: 4, task: 'get dressed', completed: 'Null', priority: 1}
       ])
     })
 }
