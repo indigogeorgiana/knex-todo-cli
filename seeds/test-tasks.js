1e9c2def1dd73008c902ca45f35dc4db31b76639
Rachael Peri <rachael.peri@gmail.com>
2018-06-11 19:59:14 +1200

    working on search

--- seeds/test-tasks.js
+++ seeds/test-tasks.js
@@ -2,11 +2,14 @@
 exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
   return knex('todos').del()
-    .then(('todos') => {
+    .then(() => {
       // Inserts seed entries
       return knex('todos').insert([
         {id: 1, task: 'vacuum'},
-        {id: 2, task: 'buy groceries'}
+        {id: 2, task: 'buy groceries'},
+        {id: 3, task: 'kick my flatmate out'},
+        {id: 4, task: 'seek revenge'},
+        {id: 5, task: 'get my shit together'}
       ])
     })
 }
