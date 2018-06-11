8d6a7e352a01457e6fd661d8c4bd6f2f9a0c58c8
Sungjun Park <sjpark731015@gmail.com>
2018-06-11 13:34:01 +1200

    Completed up to Delete task by ID

--- seeds/test-tasks.js
+++ seeds/test-tasks.js
@@ -1,6 +1,8 @@
-exports.seed = function (knex) {
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries
   return knex('todos').del()
     .then(function () {
+      // Inserts seed entries
       return knex('todos').insert([
         {id: 1, task: 'eat breakfast'},
         {id: 2, task: 'brush teeth'},
